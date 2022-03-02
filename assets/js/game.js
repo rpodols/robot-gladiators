7b2abd08219e2d351b6f1bc52fb5e7e2ddd55742
ryanp <rdpodols@gmail.com>
2022-03-02 12:20:31 -0500

    loops through enemies successfully

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,8 +18,10 @@ var enemyAttack = 12;
 }*/
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+
+while(enemyHealth > 0) {
+
+
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -70,9 +72,13 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+}
 };
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  debugger;
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
